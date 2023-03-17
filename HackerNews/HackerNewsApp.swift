abb4f61055b916f240d70434d572412ec1044c1b
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2023-03-17 00:39:24 -0700

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Ikbal Demirdoven on 2023-03-17.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
